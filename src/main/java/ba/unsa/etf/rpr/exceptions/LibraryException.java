bbacbeb756ab491deb58125e76705add6f2f5b1f
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 16:28:28 +0100

    Exception edited

--- src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.exceptions;
 
-public class LibraryException {
+/**
+ * User exception
+ * @author ejasarevic3
+ */
+
+public class LibraryException extends Exception{
+    public LibraryException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public LibraryException(String msg){
+        super(msg);
+    }
 }
