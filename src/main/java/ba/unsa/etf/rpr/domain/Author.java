d6dfd6e476a0a2a6ec13cc7af95fa6c14fc631c8
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 18:50:31 +0100

    author  set/get

--- src/main/java/ba/unsa/etf/rpr/domain/Author.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Author.java
@@ -1,7 +1,29 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * class for author table
+ */
 public class Author {
     private int id;
     private String first_name;
     private String last_name;
+
+    public int getId(){
+        return id;
+    }
+    public String getFirst_name(){
+        return first_name;
+    }
+    public String getLast_name(){
+        return last_name;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setFirst_name(String first_name){
+        this.first_name = first_name;
+    }
+    public void setLast_name(String last_name){
+        this.last_name = last_name;
+    }
 }
