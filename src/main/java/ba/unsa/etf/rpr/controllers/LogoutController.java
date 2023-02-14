1b13c2568fca01778cd6c268134ef97c428c2765
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 23:31:50 +0100

    logout button atribute added

--- src/main/java/ba/unsa/etf/rpr/controllers/LogoutController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogoutController.java
@@ -1,6 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class LogoutController implements Initializable {
+
+    @FXML
+    private Button button_logout;
+    @Override
+    public void initialize(URL location, ResourceBundle resources){}
 }
