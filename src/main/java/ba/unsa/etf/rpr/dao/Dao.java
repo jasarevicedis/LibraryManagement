750aca05302d9d2aa82b897f8e27e6700e1951cb
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 16:29:50 +0100

    fixed base dao class

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.LibraryException;
+
+import java.util.List;
+
 /**
  * Root Dao interface
  * @author Edis Jasarevic
@@ -10,31 +14,31 @@ public interface Dao<T> {
      * @param id primary key of entity
      * @return Entity from database
      */
-    T getById(int id) throws QuoteException;
+    T getById(int id) throws LibraryException;
 
     /**
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    T add(T item) throws QuoteException;
+    T add(T item) throws LibraryException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    T update(T item) throws QuoteException;
+    T update(T item) throws LibraryException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
      */
-    void delete(int id) throws QuoteException;
+    void delete(int id) throws LibraryException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<T> getAll() throws QuoteException;
+    List<T> getAll() throws LibraryException;
 }
