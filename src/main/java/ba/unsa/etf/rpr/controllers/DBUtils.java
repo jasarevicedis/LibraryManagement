2ccbe678430d8f19afdf7138e7a0c3de3e20027f
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 23:54:50 +0100

    connecting login buttons

--- src/main/java/ba/unsa/etf/rpr/controllers/DBUtils.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DBUtils.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.Parent;
+
 import java.awt.event.ActionEvent;
 
 public class DBUtils {
-    public static void changeScene(ActionEvent event, String fxmlFile, String title){}
+    public static void changeScene(ActionEvent event, String fxmlFile, String title){
+        Parent root = null;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -4,14 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
-public class LoginController implements Initializable {
+public class LoginController  {
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordPasswordField;
 
+    public void loginButtonOnAction(ActionEvent e){
+        loginMessageLabel.setText("You try to login");
+    }
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources){}
 
 }
