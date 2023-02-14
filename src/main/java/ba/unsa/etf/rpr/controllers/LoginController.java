663c0eab2011d7124c52e8e961a9178d4e415e3a
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 23:28:57 +0100

    logout controller starting

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,11 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
-    public void initialize(URL location, ResourceBundle resources)
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){}
+
 }
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogoutController.java
@@ -0,0 +1,6 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.Initializable;
+
+public class LogoutController implements Initializable {
+}
