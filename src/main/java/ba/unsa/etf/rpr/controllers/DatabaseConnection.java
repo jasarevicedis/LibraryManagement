51ba3b8e49e0c2b01067541627cb5b21da053af2
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-15 00:20:14 +0100

    connection details created

--- src/main/java/ba/unsa/etf/rpr/controllers/DatabaseConnection.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DatabaseConnection.java
@@ -8,5 +8,14 @@ public class DatabaseConnection {
     public Connection getConnection(){
         String databaseName = "";
         String databaseUser = "";
+        String databasePassword = "";
+        String url = "jdbc:mysql://localhost/"+ databaseName;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return databaseLink;
     }
 }
