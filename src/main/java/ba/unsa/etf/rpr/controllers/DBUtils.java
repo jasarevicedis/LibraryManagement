2f0668f830e90e89f32adce141ef7d5235650c45
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 23:37:24 +0100

    DButils created

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/DBUtils.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.controllers;
+
+import java.awt.event.ActionEvent;
+
+public class DBUtils {
+    public static void changeScene(ActionEvent event, String fxmlFile, String title){}
+}
