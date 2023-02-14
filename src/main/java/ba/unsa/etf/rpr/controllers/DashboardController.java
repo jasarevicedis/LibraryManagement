4ed3dfa8b1ba9d306e7e9d9d926ac16e1b2b2268
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-14 22:49:28 +0100

    JAVAFX

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/DashboardController.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.controllers;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources){}
+}
