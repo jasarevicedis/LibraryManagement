adce346ba27fd3ef9944d81ab3f2c3fad8efd024
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-15 09:23:45 +0100

    database creditentials added

--- src/main/java/ba/unsa/etf/rpr/controllers/DatabaseConnection.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DatabaseConnection.java
@@ -7,8 +7,8 @@ public class DatabaseConnection {
     public Connection databaseLink;
     public Connection getConnection(){
         String databaseName = "freedb_RPRprojekatbaza";
-        String databaseUser = "";
-        String databasePassword = "";
+        String databaseUser = "freedb_ejasarevic3";
+        String databasePassword = "yDUg2u$7HZ2f?a%";
         String url = "jdbc:mysql://localhost/"+ databaseName;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
