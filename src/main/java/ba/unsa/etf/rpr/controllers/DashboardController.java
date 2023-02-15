6ac2ea44d395441f5b59808e3d9c5ce4c278e744
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-15 21:37:43 +0100

    sidebar buttons labels added

--- src/main/java/ba/unsa/etf/rpr/controllers/DashboardController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DashboardController.java
@@ -1,12 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnBooks;
+
+    @FXML
+    private Button btnMembers;
+
+    @FXML
+    private Button btnLoans;
     @Override
     public void initialize(URL location, ResourceBundle resources){}
 }
