9d307019616945138cd39d767ae44a08db054b2f
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-15 08:36:28 +0100

    wrong on action fixed

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -12,10 +12,10 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws  Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        //primaryStage.setTitle(" Hello world !!!");
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle(" Hello world !!!");
         primaryStage.setScene(new Scene(root,USE_COMPUTED_SIZE,  USE_COMPUTED_SIZE));
         primaryStage.show();
     }
