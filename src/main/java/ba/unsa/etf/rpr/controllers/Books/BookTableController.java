8738a6f281144384b0d3b7a6af292364391d51b0
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-16 09:13:14 +0100

    Books controller updated

--- src/main/java/ba/unsa/etf/rpr/controllers/Books/BookTableController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Books/BookTableController.java
@@ -1,7 +1,25 @@
 package ba.unsa.etf.rpr.controllers.Books;
 
+import ba.unsa.etf.rpr.domain.Book;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class BookTableController implements Initializable {
+    @FXML
+    private TableView<Book> booksTable;
+    @FXML
+    private TableColumn<Book, String> idCol;
+    @FXML
+    private TableColumn<Book, String> titleCol;
+    @FXML
+    private TableColumn<Book, String> authorCol;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){}
 
 }
