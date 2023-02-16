3cb80081f7171fd3643b6c17149ff835c66bc40e
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-16 10:09:30 +0100

    loadDate

--- src/main/java/ba/unsa/etf/rpr/controllers/Books/BookTableController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Books/BookTableController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.awt.event.MouseEvent;
 import java.net.URL;
@@ -18,15 +19,25 @@ public class BookTableController implements Initializable {
     @FXML
     private TableColumn<Book, String> titleCol;
     @FXML
+    private TableColumn<Book, Integer> publishCol;
+    @FXML
     private TableColumn<Book, String> authorCol;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){}
+    public void initialize(URL url, ResourceBundle rb){
+        loadDate();
+    }
 
 
     @FXML
     private void getAddView(MouseEvent event){}
     @FXML
     private void refreshTable(MouseEvent event){}
+    private void loadDate(){
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        publishCol.setCellValueFactory(new PropertyValueFactory<>("PublishYear"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
 
+    }
 }
