7af40d041e7e4ff89bbc3d1fd8361e242b79c338
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-18 18:46:23 +0100

    logout added

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.LibraryException;
+
+import java.util.List;
 
 public interface BookDao extends Dao<Book>{
+
 
 }
