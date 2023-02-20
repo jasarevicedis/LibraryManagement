2703d3cf3af6ff8e2f2a0395d8c4877f74dc05ae
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-20 08:29:48 +0100

    dao factory added

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.dao;
+
+public class DaoFactory {
+    private static BookDao bookDao = null;
+    private static MemberDao memberDao = null;
+    private  static LoanDao loanDao = null;
+}
