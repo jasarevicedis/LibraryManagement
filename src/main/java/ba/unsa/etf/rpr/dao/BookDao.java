a21a9806d3fa084e338224b042a336bc29520d59
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-20 08:57:34 +0100

    search methods added

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.DBException;
+
+import java.util.List;
+
 public interface BookDao {
+    List<Book> getByTitle(String word) throws DBException;
+    List<Book> getByAuthor(String name) throws DBException;
+
+
 }
