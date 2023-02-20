7f0aa7e8cf0f7c2b8ee9a8d8e5c9f9f918c41869
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-20 08:32:00 +0100

    dao factory edit

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,7 +1,18 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.DBException;
+
 public class DaoFactory {
     private static BookDao bookDao = null;
     private static MemberDao memberDao = null;
     private  static LoanDao loanDao = null;
+
+    private DaoFactory() {};
+
+    public static BookDao bookDao() throws DBException {
+        if(bookDao == null){
+            bookDao = new BookDaoSQLImpl();
+        }
+        return bookDao;
+    }
 }
