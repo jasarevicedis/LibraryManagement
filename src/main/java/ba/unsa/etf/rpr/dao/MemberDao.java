25a6bc2ba740e050cb89b264ace2d7eb210f8685
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-20 16:23:05 +0100

    updated member dao

--- src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Member;
+import ba.unsa.etf.rpr.exceptions.DBException;
 
 public interface MemberDao extends Dao<Member>{
+
+    public void searchByFirstName(String first_name) throws DBException;
+    public void searchByListName(String last_name) throws DBException;
 }
