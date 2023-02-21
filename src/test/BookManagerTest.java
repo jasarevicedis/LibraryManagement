98c9e2d6d9cff288f173007d115ee8a19b7689fb
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-21 06:03:15 +0100

    maven dependencies added

--- src/test/BookManagerTest.java
+++ src/test/BookManagerTest.java
@@ -1,2 +1,8 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class BookManagerTest {
 }
