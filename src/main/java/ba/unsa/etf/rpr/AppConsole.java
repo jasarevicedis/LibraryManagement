07241eceb082f45e7cf1750ee5877934b3eed0e9
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-23 11:37:41 +0100

    creating command line interface

--- src/main/java/ba/unsa/etf/rpr/AppConsole.java
+++ src/main/java/ba/unsa/etf/rpr/AppConsole.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
+import org.apache.commons.cli.*;
 
 public class AppConsole {
+    private static final Option addBook = new Option("b", "add-book", false, "Adding book to the database");
+    private static final Option addMember = new Option("m", "add-member", false, "Adding member to the database");
+    private static final Option getBooks = new Option("getB", "get-books", false, "Printing all books from the database");
+    private static final Option getMembers = new Option("getM", "get-members", false, "Printing all members from the database");
+
+
+    private static Options addOptions() {
+        Options options = new Options();
+
+        options.addOption(addBook);
+        options.addOption(addMember);
+        options.addOption(getBooks);
+        options.addOption(getMembers);
+
+        return options;
+    }
 }
+
